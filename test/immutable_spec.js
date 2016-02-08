97508e78cd32624278db667c6bd468b18de2c996
Chuck Conway <charles@cconway.com>
2016-02-07 23:06:15 -0800

    expanded on immutable_spec test

--- test/immutable_spec.js
+++ test/immutable_spec.js
@@ -1,7 +1,10 @@
 import {expect} from 'chai';
+import {List} from 'immutable';
+
+//https://facebook.github.io/immutable-js/
+//http://teropa.info/blog/2015/09/10/full-stack-redux-tutorial.html
 
 describe('immutability', () => {
-
   describe('a number', () => {
 
     function increment(currentState) {
@@ -15,7 +18,29 @@ describe('immutability', () => {
       expect(nextState).to.equal(43);
       expect(state).to.equal(42);
     });
-
   });
 
+   describe('A List', () => {
+
+    function addMovie(currentState, movie) {
+      return currentState.push(movie);
+    }
+
+    it('is immutable', () => {
+      let state = List.of('Trainspotting', '28 Days Later');
+      let nextState = addMovie(state, 'Sunshine');
+
+      expect(nextState).to.equal(List.of(
+        'Trainspotting',
+        '28 Days Later',
+        'Sunshine'
+      ));
+
+      expect(state).to.equal(List.of(
+        'Trainspotting',
+        '28 Days Later'
+      ));
+    });
+
+  });
 });

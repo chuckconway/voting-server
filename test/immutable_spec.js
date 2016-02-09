db484bbe2b1bc2520abac4392b9a69fcef5bf09a
Chuck Conway <charles@cconway.com>
2016-02-09 06:55:09 -0800

    Added a new Test

--- test/immutable_spec.js
+++ test/immutable_spec.js
@@ -1,5 +1,5 @@
 import {expect} from 'chai';
-import {List} from 'immutable';
+import {List, Map} from 'immutable';
 
 //https://facebook.github.io/immutable-js/
 //http://teropa.info/blog/2015/09/10/full-stack-redux-tutorial.html
@@ -20,7 +20,7 @@ describe('immutability', () => {
     });
   });
 
-   describe('A List', () => {
+   describe('A list', () => {
 
     function addMovie(currentState, movie) {
       return currentState.push(movie);
@@ -43,4 +43,31 @@ describe('immutability', () => {
     });
 
   });
+
+   describe('a tree', () => {
+
+      function addMovie(currentState, movie){
+          return currentState.set('movies', currentState.get('movies').push(movie));
+
+      }
+
+      it('is immutable', ()=> {
+          let state = Map({
+            movies:List.of('Trainspotting', '28 Days Later')
+          });
+
+          let nextState = addMovie(state, 'Sunshine');
+
+          expect(nextState).to.equal(Map({
+            movies:List.of('Trainspotting', '28 Days Later', 'Sunshine')            
+          }));
+
+          expect(state).to.equal(Map({
+            movies:List.of(
+                'Trainspotting', '28 Days Later'
+              )
+          }));
+      });
+
+   });
 });
